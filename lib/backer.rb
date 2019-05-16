8036b408a28dc6cc0933c036de6da8245902413e
Scott <galejscott@gmail.com>
2019-05-16 15:57:10 +0100

    complete avec @Moniet

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require_relative "project.rb"
+class Backer
+    attr_reader :backed_projects, :name
+
+    def initialize(name)
+        @backed_projects = []
+        @name = name
+    end
+
+    def back_project(project)
+        @backed_projects << project
+        project.backers << self
+    end
+
+end
